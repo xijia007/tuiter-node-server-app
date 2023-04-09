6a87c35bcae1cad6de2256ee2678d058fcc11309
xijia007 <jia.xi1@northeastern.edu>
2023-04-08 19:30:01 -0700

    a9 Mongo Spring23

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const app = express();
 app.use(cors());
 app.use(express.json());
