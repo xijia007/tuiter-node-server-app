6164142f0b646d715e00ba518cb9d6f3d0910ffa
xijia007 <jia.xi1@northeastern.edu>
2023-04-08 19:06:11 -0700

    a9 Mongo Spring23

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ UserController(app);
 // });
 // app.listen(4000);
 app.listen(process.env.PORT || 4000);
-mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
+// mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/tuiter";
 mongoose.connect(CONNECTION_STRING);
